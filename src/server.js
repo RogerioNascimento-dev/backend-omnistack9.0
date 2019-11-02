ab76363a6361a82d7a8706129bcc51b76cf851ab
RogerioNascimento-dev <rogerionascimento.dev@outlook.com.br>
2019-11-01 22:43:18 -0300

    criando no model de spot um parametro visual para terornar a imagem a ser consumida publicamente

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 const cors = require('cors');
+const path = require('path');
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack9-go5rk.mongodb.net/semana9?retryWrites=true&w=majority',
 {
@@ -13,6 +14,7 @@ const app = express();
 //informa para o express que ele irá trabalhar com formato json
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname,'..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
